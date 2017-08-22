82e54e567f63957c80212bfa43d68e8d8ec159d4
liz solorio <lizsolorio211@gmail.com>
2017-08-22 15:20:33 -0700

    adding all the files 8-22

--- www/community.js
+++ www/community.js
@@ -1,8 +1,60 @@
 var database = firebase.database();
+window.onload = load;
 
+var user = sessionStorage.username;
 
-function storeThedata(){
-    var key = document.getElementById('key').value;
-    var val = document.getElementById('value').value;
-    database.ref("users/" + key).set({message : val});
+function load(){ //loads the function
+  database.ref("messages").once("value").then(function(snap){
+    var messages =snap.val();
+    if (messages != null){
+
+    for (var i =1; i<= Object.keys(messages).length; i ++){ //add the message everytime
+
+      var m = "m" + i;
+      var info = messages[m]; //variable for messages
+      var username = info.username; //variable for username
+      var message = info.msg; // variable for message
+      addMessage(info.username , info.msg); //adds the message and the username
+    }
+}
+  });
+}
+function addMessage(username , message){ //function to bring username and message together
+  var aMessage = document.createElement("p"); //makes the message into a paragraph
+  aMessage.innerHTML= username + ": " + message; //
+  var text = document.getElementById("Messages");//calles the message
+  text.appendChild(aMessage); //appends the message
+}
+
+function storeTheData(){ //stores into data base
+
+    database.ref("messages").once("value").then(function(snap){ //looking at database in messages
+      var numberOfMsg = -1;
+      var messages =snap.val();
+      if (messages != null){
+      numberOfMsg = Object.keys(messages).length + 1; //adding the number of the messages
+      }
+      else {numberOfMsg = 1}
+
+      var m = "m" + numberOfMsg; //howver many plus one
+      var Message = document.getElementById('key').value; //getting the value of the text
+      var UserName = sessionStorage.username;//keeps the username logged in to do the message thing
+      database.ref("messages/" + m).set({username: UserName , msg:Message}); //sets it into database
+      var div = document.getElementById ('Messages');
+      while (div.hasChildNodes()) { //keeps removing children
+          div.removeChild(div.lastChild);
+      }
+      load();
+
+    });
   }
+
+
+
+
+
+
+//var ref = firebase.database().ref("users");
+//ref.once("value")
+//.then(function(snapshot) {
+  //  var name = snapshot.child("users").val();
